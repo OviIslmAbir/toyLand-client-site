d767a44623a31937f27954b6ad944fec66dddfa5
Abir <abirovi481@gmail.com>
2023-05-17 21:31:26 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Main from './Layout/Main';
+import Home from './Pages/Home/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+
+      },
+      {
+
+      },
+      {
+
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+       <RouterProvider router={router} />
+  </React.StrictMode>,
+)
