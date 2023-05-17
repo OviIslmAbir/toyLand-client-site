ae79bcd0bf360305e6cbd027f1e6d473d58aed8f
Abir <abirovi481@gmail.com>
2023-05-18 01:32:28 +0600

    sixth commit

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import ToyCategory from '../ToyCategory/ToyCategory';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <ToyCategory></ToyCategory>
         </div>
     );
 };
