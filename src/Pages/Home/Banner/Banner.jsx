e77f6e3434f0d2fe2f181cab11047b7e89870a62
Abir <abirovi481@gmail.com>
2023-05-17 22:27:50 +0600

    second commit

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './banner.css'
+const Banner = () => {
+    return (
+        <div className='container my-3 h'>
+            <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active" data-bs-interval="10000">
+                      <img src="../../../../public/img/banner-1.jpg" style={{height:"580px"}} className="d-block w-100 rounded" alt="..."/>
+                        <div className="carousel-caption d-flex align-items-center justify-content-center">
+                            <div>
+                                <h2>Welcome to <span className='text-danger'>Toy Land</span></h2>
+                                <p>Get an exclusive toys at comparative price</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item" data-bs-interval="2000">
+                      <img src="../../../../public/img/banner-2.jpg" style={{height:"580px"}} className="d-block w-100 rounded" alt="..."/>
+                       <div className="carousel-caption d-flex align-items-center justify-content-center">
+                            <div>
+                                <h2>Welcome to <span className='text-danger'>Toy Land</span></h2>
+                                <p>Get an exclusive toys at comparative price</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                       <img src="../../../../public/img/banner-3.jpg" style={{height:"580px"}} className="d-block w-100 rounded" alt="..."/>
+                       <div className="carousel-caption d-flex align-items-center justify-content-center">
+                            <div>
+                                <h2>Welcome to <span className='text-danger'>Toy Land</span></h2>
+                                <p>Get an exclusive toys at comparative price</p>
+                            </div>
+                        </div>
+                    </div> rounded
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary container">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to='/'>Toy Land</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link me-3 fw-bold" aria-current="page">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="" className="nav-link me-3 fw-bold">All Toys</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="" className="nav-link me-3 fw-bold">My Toys</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="" className="nav-link me-3 fw-bold">Add A Toy</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="" className="nav-link me-3 fw-bold ">Blogs</Link>
+                        </li>
+                    </ul>
+                    <div>
+                       <Link to='/login'><button className='btn bg-danger text-white me-2'>Login</button></Link>
+                       <FaUserCircle style={{width:"35px", height:"35px"}}/>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 };
 
